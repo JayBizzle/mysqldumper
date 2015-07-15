76801d5df62dd4aa49480c615f7ed7ef6e94b95c
Mark Beech <mbeech@mark-beech.co.uk>
2015-07-15 19:17:38 +0100

    Initial commit

---
+++ src/mysqldumper.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace mysqldumper\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class mysqldumper extends Command
+{
+    protected $cli;
+
+    protected $git;
+
+    public function __construct($git, $cli)
+    {
+        parent::__construct();
+        $this->git = $git;
+        $this->cli = $cli;
+    }
+
+    protected function configure()
+    {
+        $this->setName('mysqldumper')->setDescription('dump')->addArgument(
+            'env',
+            InputArgument::REQUIRED,
+            'Required option'
+        )->addArgument(
+            'destination',
+            InputArgument::OPTIONAL,
+            'optional argument?'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->git->setRepository('./');
+
+        $this->mysqldumper();
+    }
+
+    public function mysqldumper()
+    {
+        $this->out('Dumping...', 'success');
+    }
+
+    public function getRepoStatus()
+    {
+        return $this->git->status();
+    }
+
+    public function out($message, $style = 'info')
+    {
+        switch ($style) {
+        case 'info':
+            $this->cli->blue($message);
+            break;
+        case 'success':
+            $this->cli->green($message);
+            break;
+        case 'error':
+            $this->cli->red($message);
+            break;
+        }
+    }
+}
