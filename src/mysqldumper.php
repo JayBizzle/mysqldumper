0efae0b41c3884166d3f165f4a8373b1da702e71
Max King <max@gradcracker.com>
2015-07-16 13:57:00 +0100

    Add Flysystem local to manage folder creation/naming

--- src/mysqldumper.php
+++ src/mysqldumper.php
@@ -6,6 +6,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
 
 class mysqldumper extends Command
 {
@@ -38,7 +40,11 @@ class mysqldumper extends Command
 
     public function mysqldumper()
     {
-        $config = json_decode(file_get_contents('config.json'));
+        $start_dump = date('YmdHi');
+        $config     = json_decode(file_get_contents('config.json'));
+        $adapter    = new Local(__DIR__.'/../dump');
+        $filesystem = new Filesystem($adapter);
+        $filesystem->createDir('/'.$start_dump);
         try {
             $conn = new \PDO("mysql:host=".$config->host.";dbname=".$config->db, $config->user, $config->pass);
             $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -53,7 +59,7 @@ class mysqldumper extends Command
             foreach($table_list as $table)
             {
                 $progress->advance(1, '<light_green>('.$i.' of '.$table_count.') Dumping '.$table['Tables_in_'.$config->db].'</light_green>');
-                exec('/Applications/MAMP/Library/bin/mysqldump --user='.$config->user.' --password='.$config->pass.' --host='.$config->host.' '.$config->db.' '.$table['Tables_in_'.$config->db].' | gzip > dump/'.$table['Tables_in_'.$config->db].'.sql.gz', $output);
+                exec('/Applications/MAMP/Library/bin/mysqldump --user='.$config->user.' --password='.$config->pass.' --host='.$config->host.' '.$config->db.' '.$table['Tables_in_'.$config->db].' | gzip > dump/'.$start_dump.'/'.$table['Tables_in_'.$config->db].'.sql.gz', $output);
                 $i++;
             }
         }
@@ -62,11 +68,6 @@ class mysqldumper extends Command
         }
 
         $this->out('completed', 'success');
-        
-        // foreach($tables as $table)
-        // {
-        //     exec('/Applications/MAMP/Library/bin/mysqldump --user=... --password=... --host=... ... | gzip > dumpzip2/'.$table->Tables_in_DATABASENAME.'.sql.gz', $output);
-        // }
     }
 
     public function out($message, $style = 'info')
